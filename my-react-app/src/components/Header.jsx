a12ab08ca6f3cda5f8d79f18fe888e1f67b62d4c
zdebar <157598603+zdebar@users.noreply.github.com>
2025-01-30 18:20:23 +0100

    Icon Sizes Fixed 2

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -7,7 +7,7 @@ import IconFolder from "./IconFolder"
 function Header() {
   return (
     <header className="header">
-      <Icon IconImage={IconBell}/>
+      <Icon IconImage={IconBell} style={{ width: "23px" }}/>
       <nav className='header__right'>
         <Icon IconImage={IconFolder} style={{ width: "18px" }}/>
         <Icon IconImage={IconUser}/>
