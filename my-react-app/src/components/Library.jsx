b505bc29cc7721c4b88bbbdff55e1da022357bd3
zdebar <157598603+zdebar@users.noreply.github.com>
2025-02-07 13:20:20 +0100

    Library Start

--- my-react-app/src/components/Library.jsx
+++ my-react-app/src/components/Library.jsx
@@ -1,3 +1,35 @@
-export default function Library () {
-    return <h1>Library</h1>;
-} 
+import { useEffect, useState } from "react";
+
+export default function Library() {
+    const [lectures, setLectures] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchedLectures = [
+            {
+                "id": 1,
+                "name": "Představování"
+            },
+            {
+                "id": 2,
+                "name": "Rodina"
+            }
+        ];
+        setLectures(fetchedLectures);
+        setLoading(false);
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <ul>
+                {lectures.map((lecture) => (
+                    <li key={lecture.id}>{lecture.id} : {lecture.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
