e7896d41458276af59f47cd8dc38d24858f71884
zdebar <157598603+zdebar@users.noreply.github.com>
2025-02-10 17:48:35 +0100

    Intalling Typescript

--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import './main.css'
-import App from './App.jsx'
+import App from './App.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
